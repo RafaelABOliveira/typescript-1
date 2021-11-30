c085470345be63325ee2864e0e67c280f43e2e67
Athena Garcia <rafaelntn766@gmail.com>
2021-11-30 19:57:57 -0300

    criaDe

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,7 +20,12 @@ export class NegociacaoController {
   }
 
   public adiciona(): void {
-    const negociacao = this.criaNegociacao();
+    const negociacaoTemp = new Negociacao(null, 0, 0);
+    const negociacao = negociacaoTemp.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
     if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
       return;
@@ -35,14 +40,6 @@ export class NegociacaoController {
     return (
       data.getDay() > DiaDaSemana.DOMINGO && data.getDay() < DiaDaSemana.SABADO
     );
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ","));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
   }
 
   private limparFormulario(): void {
